420aa791bf636353fdc4e6b1f3c7e36b8df32b5d
Sylvan Lavallee <geckoweb.webmaster@gmail.com>
2018-09-26 19:15:46 +0200

    travail du cours du 26 nov 2018

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('hello world')
+console.log('hello world');
+
+var $menu = document.querySelector('.menu_chat');
+var $icon = document.querySelector('.icon')
+
+$menu.addEventListener('click', function () {
+   $icon.classList.toggle('menu');
+   $icon.classList.toggle('close');
+});
