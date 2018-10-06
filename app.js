28695fd1572941a4dc5e3bc3fd32afaa8b5c8f4b
Sylvan Lavallee <geckoweb.webmaster@gmail.com>
2018-10-06 17:27:36 +0200

    emoticones and working messages

--- app.js
+++ app.js
@@ -1,9 +1,56 @@
 console.log('hello world');
 
+//----------------------- Chat menu -------------------------
+
 var $menu = document.querySelector('.menu_chat');
-var $icon = document.querySelector('.icon')
+var $icon = document.querySelector('.icon');
 
 $menu.addEventListener('click', function () {
    $icon.classList.toggle('menu');
    $icon.classList.toggle('close');
+});
+
+//--------------------- Smiley popup ------------------------
+
+var $smile_menu = document.querySelector('.smile_menu_button');
+var $smile_popup = document.querySelector('.smile_popup');
+var $arrow_down = document.querySelector('.arrow_down');
+
+$smile_menu.addEventListener('click', function () {
+   $smile_popup.classList.toggle('hidden');
+   $arrow_down.classList.toggle('hidden');
+   $smile_popup.classList.toggle('');
+   $arrow_down.classList.toggle('');
+});
+
+//----------------- Add smiley to textarea ------------------
+
+var $textarea = document.querySelector('.text_area textarea');
+var $smiley_1 = document.querySelector('#smiley_1');
+
+$smiley_1.addEventListener('click', function () {
+    $textarea.value += '😊';
+});
+
+var $textarea = document.querySelector('.text_area textarea');
+var $smiley_2 = document.querySelector('#smiley_2');
+
+$smiley_2.addEventListener('click', function () {
+    $textarea.value += '🤣';
+});
+
+//--------------------- Send message -------------------------
+
+var $send = document.querySelector('.send');
+var $messages = document.querySelector('.messages');
+
+$send.addEventListener('click', function () {
+    var $newDiv = document.createElement('div');
+
+    $messages.appendChild($newDiv);
+    $newDiv.classList.add('txt_send');
+
+    $newDiv.innerHTML = $textarea.value;
+    $textarea.value = '';
+
 });
